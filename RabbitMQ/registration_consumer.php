a7c9768dcd7d053d92cd8846e6548fc03238924a
walterg7 <walterg07203@gmail.com>
2025-02-23 18:30:31 -0500

    logic to receive message from registration form, and push it to the database (not working properly)

---
+++ RabbitMQ/registration_consumer.php
@@ -0,0 +1,54 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php'; //Ensure php-amqplib is installed
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Channel\AMQPChannel;
+
+try {
+    // Connect to DB
+    // Note: This is accessing the DB as root and assumes a database named IT490 exists. The IT490 DB should have tables created from user_auth.sql
+    $db = new mysqli("localhost", "root", "", "IT490");
+
+    // Change paramaters after mesage broker VM is designated 
+    $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+    $channel = $connection->channel();
+
+    // Declare the registration queue
+    $queue_name = 'registration_request';
+    $channel->queue_declare($queue_name, false, false, false, false);
+
+    echo "Waiting for registration request...\n";
+
+    // Process messages from queue
+    $callback = function ($msg) use ($db) {
+        // The producer's message is encoded in JSON; turn into associative array for further processing
+        $data = json_decode($msg->body, true);
+        $email = $data['email'];
+        $username = $data['username'];
+        $password = $data['password'];
+
+        // Insert data into DB
+        $stmt = $db->prepare("INSERT INTO users (email, username, password) VALUES ($email, $username, $password)");
+
+        if ($stmt->execute()) {
+            echo "User $username successfully registered and added to database.\n";
+        } else {
+            echo "Error: " . $stmt->error . "\n";
+        }
+
+        $stmt->close();
+    };
+
+    $channel->basic_consume($queue_name, '', false, true, false, false, $callback);
+
+    // Continuously listen for messages
+    while (true) {
+        $channel->wait();
+    }
+
+    $channel->close();
+    $connection->close();
+} catch (Exception $error) {
+    echo "Error: {$error->getMessage()}\n";
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '95360e9250e9358f02c2c6b83b0c255b5a6b9512',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '95360e9250e9358f02c2c6b83b0c255b5a6b9512',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'videlalvaro/php-amqplib' => array(
+            'pretty_version' => 'v2.5.2',
+            'version' => '2.5.2.0',
+            'reference' => 'eb8f94d97c8e79900accf77343dbd7eca7f58506',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../videlalvaro/php-amqplib',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
